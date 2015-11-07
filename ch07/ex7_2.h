3e8eed4f72b547fece162fb891a46828b4317271
makai <mkmk19900215@163.com>
2015-11-07 22:18:33 +0800

    update

---
+++ ch07/ex7_2.h
@@ -0,0 +1,34 @@
+//
+//  ex7_02.h
+//  Exercise 7.2
+//
+//  Created by M0Z0 on 15/10/26.
+//  Copyright (c) 2015 M0Z0. All rights reserved.
+//
+
+#ifndef EX7_2_H
+#define EX7_2_H
+
+#include<string>
+
+struct Sales_data
+{
+private:
+    std::string bookNo;
+    unsigned units_sold = 0;
+    double revenue = 0.0;
+
+public:
+    inline std::string isbn()const{ return bookNo; }
+    Sales_data& combine(Sales_data&);
+};
+
+Sales_data& combine(Sales_data& rhs)
+{
+    units_sold += rhs.units_sold;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+#endif // EX7_2_H
+
