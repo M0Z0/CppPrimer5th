f610a6c249af4d15054f1ebf83660603d7885cb8
makai <mkmk19900215@163.com>
2016-07-31 23:48:32 +0800

    ud

---
+++ ch12/ex12_27.h
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <memory>
+#include <set>
+#include <vector>
+#include <map>
+#include <string>
+
+using namespace std;
+
+class QueryResult;
+class TextQuery{
+public:
+	using line_no = vector<string>::size_type;
+	TextQuery(ifstream&);
+	QueryResult query(const string&) const;
+private:
+	shared_ptr<vector<string>> file;
+	map<string, shared_ptr<set<line_no>>> wm;
+};
+
+TextQuery::TextQuery(ifstream &is) : file(new vector<string>)
+{
+	string text;
+	while (getline(is, text)) {
+		file->push_back(text);
+		int n = file->size() - 1;
+		istringstream line(text);
+		string word;
+		while (line>>word) {
+			auto &lines = wm[word];
+			if (!lines)
+			{
+				lines.reset(new set<line_no>);
+			}
+			lines->insert(n);
+		}
+	}
+}
