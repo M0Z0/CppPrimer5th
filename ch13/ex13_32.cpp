e9842f3dfcba7b1da25ecbccb24ee9babe855817
makai <mkmk19900215@163.com>
2016-08-08 22:42:21 +0800

    update

--- ch13/ex13_32.cpp
+++ ch13/ex13_32.cpp
@@ -3,5 +3,46 @@
 void Message::save(Folder &f)
 {
 	folders.insert(&f);
-	f.addMsg(this);
+	//f.addMsg(this);
+}
+
+void Message::remove(Folder &f)
+{
+	folders.erase(&f);
+	//f.remMsg(this);
+}
+
+void Message::add_to_Folders(const Message &m)
+{
+	for (auto f : m.folders)
+	{
+		//f->addMsg(this);
+	}
+}
+
+Message::Message(const Message &m) : contents(m.contents), folders(m.folders)
+{
+	add_to_Folders(m);
+}
+
+void Message::remove_from_Folders()
+{
+	for (auto f : folders)
+	{
+		//f->remMsg(this);
+	}
+}
+
+Message::~Message()
+{
+	remove_from_Folders();
+}
+
+Message& Message::operator=(const Message &rhs)
+{
+	remove_from_Folders();
+	this->contents = rhs.contents;
+	this->folders = rhs.folders;
+	add_to_Folders(rhs);
+	return *this;
 }
